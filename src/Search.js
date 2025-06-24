4bd786d5405f21793006f9fbe86eec781fff5fce
yuki <38411009+yukijina@users.noreply.github.com>
2025-06-24 06:38:07 -0700

    updated api url. fetching data from forecast, not single day

--- src/Search.js
+++ src/Search.js
@@ -2,28 +2,32 @@ import React, { useState } from 'react';
 import './Search.css';
 import axios from 'axios';
 import Weather from './Weather';
+import Forecast from './Forecast';
 
 export default function Search() {
   const [city, setCity] = useState(null);
   const [data, setData] = useState({});
+  const [forecasts, setForecasts] = useState([]);
 
   function showWeather(response) {
-    // console.log(response.data);
+    // console.log(response.data.daily);
     setData({
       city: response.data.city,
-      description: response.data.condition.description,
-      temperature: Math.round(response.data.temperature.current),
-      humidity: response.data.temperature.humidity,
-      wind: response.data.wind.speed,
-      icon_url: response.data.condition.icon_url,
+      country: response.data.country,
+      description: response.data.daily[0].condition.description,
+      temperature: Math.round(response.data.daily[0].temperature.day),
+      humidity: response.data.daily[0].temperature.humidity,
+      wind: response.data.daily[0].wind.speed,
+      icon_url: response.data.daily[0].condition.icon_url,
     });
+    setForecasts(response.data.daily);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
     const apiKey = '2f5896dd4cc0cdo340203tba4fba205f';
-    const url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    const url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
     axios.get(url).then(showWeather);
   }
 
@@ -46,6 +50,7 @@ export default function Search() {
         <input type='submit' value='Search' className='Search-submit'></input>
       </form>
       <Weather data={data} />
+      <Forecast forecasts={forecasts} />
     </div>
   );
 }
